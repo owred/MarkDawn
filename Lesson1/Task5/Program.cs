89851d3a364a46306ea1687d5e7ae682b36b1384
owred <owred@yandex.ru>
2023-12-19 23:18:30 +0300

    add Lesson1:task5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+    //Console.WriteLine(i); // Для вывода в столбик
+    Console.Write(i);
+    Console.Write(' '); // Для пробелов между выводимыми в строку значениями
+    i = i + 1;
+}
