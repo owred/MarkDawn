027a7c52269f6e716740f14bf6242bfb9393e473
owred <owred@yandex.ru>
2023-12-25 21:20:13 +0300

    run leson2:Htask1

--- Lesson_2M/HTask1/Program.cs
+++ Lesson_2M/HTask1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое значение: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no" );
+}
