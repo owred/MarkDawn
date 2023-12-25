19ed4b526e89119e95cde215ed3ce679f8b0b3ed
owred <owred@yandex.ru>
2023-12-25 20:08:38 +0300

    run leson2:task2

---
+++ Lesson_2M/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и возводит вторуюцифру этого числа в степень равнуютретей цифре
+
+Console.WriteLine("Введите значение: ");
+int a = int.Parse(Console.ReadLine()!);
+int SecondNumb = a / 10 % 10;
+int ThirdNumb = a % 10;
+Console.WriteLine(Math.Pow(SecondNumb,ThirdNumb));
