c71dca418e973ef5593f3bc3ba01ca8da09ed844
owred <owred@yandex.ru>
2023-12-25 21:05:55 +0300

    run leson2:task4

---
+++ Lesson_2M/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит 3 с конца цифру заданного числа или сообщает что третьей цифры нет
+
+Console.WriteLine("Введите значение: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(a / 100 % 10);
+}
