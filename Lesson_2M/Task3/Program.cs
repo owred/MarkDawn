78159144bb9c59941e09a7e806210b7d1ffa0e3b
owred <owred@yandex.ru>
2023-12-25 20:49:43 +0300

    run leson2:task3

---
+++ Lesson_2M/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. если второе число не кратно первому , то программа выводит остаток от деления
+
+Console.WriteLine("Введите первое значение: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе значение: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a % b == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no, " + a % b);
+}
