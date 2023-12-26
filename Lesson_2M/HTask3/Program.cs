1c88d42a5a33a8eedec07647adb46fc60df813b4
owred <owred@yandex.ru>
2023-12-26 04:18:30 +0300

    run leson2:Htask3

---
+++ Lesson_2M/HTask3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа
+
+Console.WriteLine("Введите целое число из отрезка [10, 99]: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number >= 10 && number <= 99)
+{
+    int firstNum = number /10;
+    int secondNum = number % 10;
+
+    int maxNum = firstNum > secondNum ? firstNum:secondNum;
+    Console.WriteLine(maxNum);
+}
+else
+{
+    Console.WriteLine("Введено не корректное число");
+}
