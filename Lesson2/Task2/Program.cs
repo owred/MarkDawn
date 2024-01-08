c224f4d8bf2791e10a7d9c7a3d4db86480c97942
owred <owred@yandex.ru>
2024-01-09 01:11:58 +0300

    Lesson2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+//while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.write(arr[i]);
+    //Console.write(' ');
+    Console.Write($"{arr[i]} "); //Тоже самое действие что и в 8,9 стороке
+    i = i + 1;
+}
