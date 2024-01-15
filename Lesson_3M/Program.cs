998d02f1f81854ad47e37001ab60b772fa4158ef
owred <owred@yandex.ru>
2024-01-15 05:09:47 +0300

    Lesson_3M:Tasck1

---
+++ Lesson_3M/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве. Программа должна выдать ответ: Да/Нет.
+
+int[] arr = {1,3,4,19,3,-4,5};
+int a = int.Parse(Console.ReadLine()!);
+bool b = false;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(a == arr[i])
+    b = true;
+}
+if(b)
+Console.Write("Yes");
+
+else
+Console.Write("No");
