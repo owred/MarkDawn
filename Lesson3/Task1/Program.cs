7a4ddcb20046e695c338065933eb4510af08f93b
owred <owred@yandex.ru>
2024-01-18 10:00:55 +0300

    Lesson3:Tasck1

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычислить значение формулы (a * b)/(c + d), a,b,c,d, - некоторые целые числа. Решение оформить в виде функции
+
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b; // числитель
+    int denomenator = c + d; // знаменатель
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
