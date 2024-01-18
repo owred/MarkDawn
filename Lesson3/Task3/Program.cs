3f664f2c8bd7e881a2519174937251a4f85f3c4d
owred <owred@yandex.ru>
2024-01-18 12:27:09 +0300

    Lesson3:Tasck3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Функция обнуляющая элементы массива
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArry(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArry(array);
