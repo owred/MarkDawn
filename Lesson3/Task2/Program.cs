43fca12420be1bfb5ac75860851658cf7c1e43d3
owred <owred@yandex.ru>
2024-01-18 12:02:36 +0300

    Lesson3:Tasck2

--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод на экран квадратов чисел от 1 до N
+
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
