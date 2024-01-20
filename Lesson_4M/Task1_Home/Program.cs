0d83364bd06059d6996f0040594e52ea136aa1c5
owred <owred@yandex.ru>
2024-01-20 11:33:57 +0300

    Lesson4:Tasck1_Houme

---
+++ Lesson_4M/Task1_Home/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true) 
+        {
+            Console.Write("Введите число или 'q' для выхода: ");
+            string input = Console.ReadLine()!; 
+
+            if (input == "q") 
+            {
+                break;
+            }
+
+            int number;
+            if (int.TryParse(input, out number)) 
+            {
+                int sum = 0;
+                int originalNumber = number; 
+
+                while (number > 0)
+                {
+                    sum += number % 10; 
+                    number /= 10; 
+                }
+
+                if (sum % 2 == 0) 
+                {
+                    Console.WriteLine($"Сумма цифр числа {originalNumber} четная. [STOP]");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+            }
+        }
+    }
+}
