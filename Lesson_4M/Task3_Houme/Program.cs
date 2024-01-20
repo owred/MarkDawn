b98009997fa7fa7acf38d6c62bd4b41eea818e56
owred <owred@yandex.ru>
2024-01-20 11:42:00 +0300

    Lesson4:Tasck3_Houme

---
+++ Lesson_4M/Task3_Houme/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 5, 6, 7, 8 }; 
+        int temp;
+
+        Console.Write("Исходный массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+
+        Console.Write("\nПеревернутый массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
