7ae459f417e338cc3b7f6063462d429247ca5546
owred <owred@yandex.ru>
2024-01-24 23:03:15 +0300

    Lesson4:Tasck2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Функция CreateMatrix
+// Что будет делать функция? Какие данные для этого ей понадобятся? Что будет возвращать функция?
+
+int[,] CreateMatrix (int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+    
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i,j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+
+}
+
+void ShowMatrix(int[,] matrix)
+{
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} "); // Интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
