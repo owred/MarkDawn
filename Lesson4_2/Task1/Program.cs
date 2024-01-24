7a030aace418bff84716085a243cd6cdfa567618
owred <owred@yandex.ru>
2024-01-25 00:51:30 +0300

    Lesson4_2:Tasck1

--- Lesson4_2/Task1/Program.cs
+++ Lesson4_2/Task1/Program.cs
@@ -4,9 +4,9 @@
 string GetLettersFromStr(string s)
 {
     string letters = "";
-    foreach(char e in s)
+    foreach (char e in s)
     {
-        if(char.IsAsciiLetter(e) == true)
+        if (char.IsAsciiLetter(e) == true)
         {
             letters = letters + e;
         }
