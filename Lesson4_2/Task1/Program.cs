f49a39419adfce3e1e119458f1acee5f152ddab6
owred <owred@yandex.ru>
2024-01-25 00:38:44 +0300

    Lesson4_2:Tasck1

---
+++ Lesson4_2/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считать с консоли строку состоящую из цифр и латинских букв
+// Сформировать новую строку, состоящую из букв исходной строки
+
+string GetLettersFromStr(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string letters = GetLettersFromStr(str);
+Console.WriteLine(letters);
