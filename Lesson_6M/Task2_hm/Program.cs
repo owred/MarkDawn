3148d119527ca74148ad84bff4aa5bdd257a22a5
owred <owred@yandex.ru>
2024-01-30 21:03:46 +0300

    Lesson_6M:Tasck2_hm

---
+++ Lesson_6M/Task2_hm/Program.cs
@@ -0,0 +1,14 @@
+﻿// Замена заглавных букв на строчные в строке
+
+public class Task2
+{
+public static void Main(string[] args)
+{
+// Входная строка со смешанными буквами обоих регистров
+string input = "aBcD1ef!-";
+// Преобразование всех заглавных букв в строчные
+string result = input.ToLower();
+// Вывод результата
+Console.WriteLine(result);
+}
+}
