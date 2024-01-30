865c1d44fbf594374151ada6309c2d5fe7ca425b
owred <owred@yandex.ru>
2024-01-30 19:01:08 +0300

    Lesson_6M:Tasck2

---
+++ Lesson_6M/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран.
+
+char[] strToCharArr(string str)
+{
+    int Length = str.Length;
+    char[] arr = new char[Length];
+    for (int i = 0; i < Length; i++)
+        arr [i] = str[i];
+    return arr;
+}
+string str1 = Console.ReadLine()!;
+char[] arr1 = strToCharArr(str1);
+Console.WriteLine(arr1);
+foreach (var item in arr1)
+{
+    Console.Write(item + " ");
+}
