2274b254ef9cefeeb39b77922b95c5d635f06368
owred <owred@yandex.ru>
2024-01-30 18:00:59 +0300

    Lesson_6M:Tasck1

---
+++ Lesson_6M/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из символов этого массива.
+
+string concat(char[] mass)
+{
+    string buf = "";
+    for(int i = 0; i < mass.Length; i++)
+    {
+        buf = buf + mass[i];
+    }
+    return buf;
+}
+char[] charArr = { 'H','e','l','l','o' };
+string res = concat(charArr);
+Console.WriteLine(res);
