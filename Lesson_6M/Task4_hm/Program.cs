69d025df6c03508f4e7e6b93e02c37a35abe797f
owred <owred@yandex.ru>
2024-01-30 21:10:04 +0300

    Lesson_6M:Tasck4_hm

---
+++ Lesson_6M/Task4_hm/Program.cs
@@ -0,0 +1,24 @@
+﻿// Обратный порядок слов в строке
+
+public class Task4_hm
+{
+public static void Main(string[] args)
+{
+// Входная строка со словами, разделенными пробелами
+string input = "Hello my world";
+// Вызов метода для обращения порядка слов в строке
+string result = ReverseWords(input);
+// Вывод результата
+Console.WriteLine(result);
+}
+// Метод для обращения порядка слов в строке
+public static string ReverseWords(string str)
+{
+// Разделение строки на слова
+string[] words = str.Split(' ');
+// Обращение порядка слов
+Array.Reverse(words);
+// Соединение слов обратно в строку с пробелами
+return string.Join(" ", words);
+}
+}
