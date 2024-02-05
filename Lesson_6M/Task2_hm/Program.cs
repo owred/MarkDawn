6d464c8ae165c18d7166d232a66c093ee31359d9
owred <owred@yandex.ru>
2024-02-05 08:34:02 +0300

    Lesson_7M:Tasck1

--- Lesson_6M/Task2_hm/Program.cs
+++ Lesson_6M/Task2_hm/Program.cs
@@ -2,13 +2,24 @@
 
 public class Task2_hm
 {
-public static void Main(string[] args)
+    public static void Main(string[] args)
+    {
+        // Входная строка со смешанными буквами обоих регистров
+        string input = "aBcD1ef!-";
+        // Преобразование всех заглавных букв в строчные
+        string result = input.ToLower();
+        // Вывод результата
+        Console.WriteLine(result);
+    }
+}
+
+string LowUp(string word)
 {
-// Входная строка со смешанными буквами обоих регистров
-string input = "aBcD1ef!-";
-// Преобразование всех заглавных букв в строчные
-string result = input.ToLower();
-// Вывод результата
-Console.WriteLine(result);
+    return word.ToLower();
 }
-}
+
+Console.Write("Enter some text: ");
+string text = Console.ReadLine()!;
+
+string res = LowUp(text);
+Console.WriteLine(res);
