8446f86f1058f962b265d9a3079d4f2ce3d4839b
owred <owred@yandex.ru>
2024-02-05 09:43:34 +0300

    Lesson_7M:Tasck3

---
+++ Lesson_7M/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+
+string str1 = "aeyiou";
+void Conson(string word, int i = 0)
+{
+    if(i == word.Length) return;
+    if(str1.Contains(char.ToLower(word[i]))== false)
+    Console.Write(word[i] + " ");
+    Conson (word, i + 1);
+    
+}
+Console.WriteLine("Enter letters: ");
+string str2 = Console.ReadLine ()!;
+Conson (str2);
