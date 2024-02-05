93835969b42c7ef784fc614f4380b245b442714b
owred <owred@yandex.ru>
2024-02-05 11:03:20 +0300

    IC:Tasck2_2

---
+++ IC/Task2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void rec(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m %2 == 0)
+    {
+        Console.Write($"{ m}, ");
+    }
+    rec(m + 1, n);
+
+}
+rec(m, n);
