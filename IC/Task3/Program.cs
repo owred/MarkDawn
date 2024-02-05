d1197f6d789e526236a3ba1e858788e4ce17f20e
owred <owred@yandex.ru>
2024-02-05 11:47:04 +0300

    IC:Tasck3

--- IC/Task3/Program.cs
+++ IC/Task3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+System.Console.WriteLine("Задача 3: Задайте произвольный массив. Выведете его элементы,");
+System.Console.WriteLine("начиная с конца. Использовать рекурсию, не использовать циклы.");
+int [] array = CreateArray(10,10,0);
+System.Console.WriteLine($"Произвольный массив => [{PrintArray(array)}]");
+System.Console.WriteLine($"Перевернутый массив => [{PrintArray(ArraySwap(array,array.Length-1))}]");
+
